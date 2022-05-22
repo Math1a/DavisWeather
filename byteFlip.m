15e2316fb6a6965d1d24f36a046bc460c256e9cb
Math1a <mathia.segre@gmail.com>
2022-05-22 17:06:05 +0000

    Solved communication issue (Finally!)
    Problem was way simpler than I thought...
    I forgot to tell the script that combines bytes that the bytes were 8
    bits long... So it deleted the leading zeros (honestly I am surprised it
    didn't cause more errors)
    
    Sensors shoud still be calibrated!
    
    Some useful commands I learned while debugging:
    "NEWSETUP" - It re-initializes the console after making certain
    configuration changes
    "RXCHECK" - It sends the Console Diagnostics report. The following
    values are sent on one line as a text string: total packets received,
    total packets missed, number of resynchronizations, the largest number
    of packets received in a row, and the number of CRC errors detected.

--- byteFlip.m
+++ byteFlip.m
@@ -1,5 +1,5 @@
 function [ushort] = byteFlip(bytes)
 
-ushort = bin2dec(string(dec2bin(bytes(2))) + string(dec2bin(bytes(1))));
+ushort = bin2dec(string(dec2bin(bytes(2), 8)) + string(dec2bin(bytes(1), 8)));
 
 end
